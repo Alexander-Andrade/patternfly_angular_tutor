45e4d0945408c0df307cf6cf8c4d21fd012b8bca
Alexander-Andrade <veimar.94@gmail.com>
2017-01-17 19:00:57 +0300

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get  'static_pages/index'
   get  'static_pages/index1'
   get  'static_pages/index2'
+  get  'static_pages/index3'
+  get  'static_pages/index4'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
