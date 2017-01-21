4424a9389f6c1b2a54b64aeed92cfe5a4e702f66
Alexander-Andrade <veimar.94@gmail.com>
2017-01-21 17:01:24 +0300

    change coffescript to javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,224 @@
 //= require turbolinks
 //= require_tree .
 
+
+
+
+resourceApp = angular.module('resource', ['ngRoute','ngResource','patternfly.charts','patternfly.card','templates']);
+
+resourceApp.config( function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '_resource_view.html',
+            controller: 'rootCtrl'
+        })
+        .when('/locations', {
+            templateUrl: '_resource_view.html',
+            controller: 'locationsCtrl'
+        })
+        .when('/locations/:location_id', {
+            templateUrl: '_resource_view.html',
+            controller: 'locationsCtrl'
+        })
+        .when('/locations/:location_id/departments', {
+            templateUrl: '_resource_view.html',
+            controller: 'departmentsCtrl'
+        })
+        .when('/locations/:location_id/departments/:department_id', {
+            templateUrl: '_resource_view.html',
+            controller: 'departmentsCtrl'
+        })
+        .when('/locations/:location_id/departments/:department_id/divisions', {
+            templateUrl: '_resource_view.html',
+            controller: 'divisionsCtrl'
+        })
+});
+
+resourceApp.service('resourcesServ', function () {
+    this.root = {
+        name: 'Home...',
+        price: {
+            current: 3000,
+            next: 6000
+        },
+        resources: [
+            {
+                title: 'SVM',
+                reserved_perc: 80,
+                used_perc: 30,
+                reserved: 200,
+                total: 230
+            },
+            {
+            title: 'Storage',
+                reserved_perc: 90,
+                used_perc: 60,
+                reserved: 10,
+                total: 15
+            },
+            {
+                title: 'Hours',
+                    reserved_perc: 100,
+                    used_perc: 30,
+                    reserved: 40,
+                    total: 50
+            }
+        ],
+        locations: [{
+            name: 'Location1',
+            price: {
+                current: 10,
+                next: 25
+            },
+            resources: [
+                {
+                    title: 'SVM',
+                    reserved_perc: 80,
+                    used_perc: 30,
+                    reserved: 200,
+                    total: 230
+                },
+                {
+                    title: 'Storage',
+                    reserved_perc: 90,
+                    used_perc: 60,
+                    reserved: 10,
+                    total: 15
+                },
+                {
+                    title: 'Hours',
+                    reserved_perc: 100,
+                    used_perc: 30,
+                    reserved: 40,
+                    total: 50
+                }
+            ],
+            departments: [{
+                name: 'Department1',
+                price: {
+                    current: 10,
+                    next: 25
+                },
+                resources: [
+                    {
+                        title: 'SVM',
+                        reserved_perc: 80,
+                        used_perc: 30,
+                        reserved: 80,
+                        total: 100
+                    },
+                    {
+                        title: 'Storage',
+                        reserved_perc: 90,
+                        used_perc: 60,
+                        reserved: 9,
+                        total: 10
+                    },
+                    {
+                        title: 'Hours',
+                        reserved_perc: 100,
+                        used_perc: 30,
+                        reserved: 20,
+                        total: 30
+                    }
+                ]
+                },
+                {
+                    name: 'Department2',
+                        price: {
+                    current: 10,
+                        next: 25
+                    },
+                    resources: [{
+                            title: 'SVM',
+                            reserved_perc: 80,
+                            used_perc: 30,
+                            reserved: 80,
+                            total: 100
+                        },
+                        {
+                            title: 'Storage',
+                            reserved_perc: 90,
+                            used_perc: 60,
+                            reserved: 9,
+                            total: 10
+                        },
+                        {
+                            title: 'Hours',
+                            reserved_perc: 100,
+                            used_perc: 30,
+                            reserved: 20,
+                            total: 30
+                        }
+                    ]
+                }
+
+            ]
+        }
+        ]
+    }
+});
+
+
+resourceApp.controller('rootCtrl', ['$scope','$routeParams','$location','resourcesServ', function($scope, $routeParams,$location,resourcesServ){
+    console.log(resourcesServ.root)
+}]);
+
+
+resourceApp.controller('locationsCtrl', ['$scope','$routeParams','resourcesServ', function($scope, $routeParams,resourcesServ) {
+
+}]);
+
+resourceApp.controller('departmentsCtrl', ['$scope','$routeParams','resourcesServ', function($scope, $routeParams,resourcesServ) {
+
+}]);
+
+resourceApp.directive('resourcesCard', function() {
+    return {
+        templateUrl: '_resources_card.html',
+        scope: {
+            title: '@',
+            price: '=',
+            progressBarsData: '='
+        }
+    };
+});
+
+resourceApp.directive('resourceProgress', function() {
+    return {
+        templateUrl: '_resource_progress.html',
+        scope: {
+            icon: '@',
+            title: '@',
+            data: '=',
+            modalData: '='
+        }
+    }
+});
+
+resourceApp.directive('resourceModal', function() {
+    return {
+        templateUrl: '_resource_modal.html',
+        scope: {
+            data: '='
+        }
+    }
+});
+
+resourceApp.directive('resourceBreadcrumb', function() {
+    return {
+        templateUrl: '_resource_breadcrumb.html',
+        scope: {
+            path: '@',
+            name: '@'
+        }
+    }
+});
+
+$('#myModal').on('shown.bs.modal', function() {
+    $('#myInput').focus()
+});
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
