def0cabded965eea69bdbbeefaaba930f6ee7254
Alexander-Andrade <veimar.94@gmail.com>
2017-01-21 15:23:55 +0300

    include resource and route to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require angular
+//= require angular-resource
+//= require angular-route
 //= require angular-rails-templates
 //= require_tree ./templates
 //= require patternfly
