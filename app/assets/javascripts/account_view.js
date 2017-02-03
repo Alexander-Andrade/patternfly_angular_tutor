4b80704f6bbd3e8480b1b65384041fd86248c8a7
Alexander-Andrade <veimar.94@gmail.com>
2017-02-03 14:42:33 +0300

    hell

--- app/assets/javascripts/account_view.js
+++ app/assets/javascripts/account_view.js
@@ -2,26 +2,78 @@
 //     'use strict';
     var resourceApp = angular.module('app', ['ui.router', 'ngResource', 'patternfly.charts', 'patternfly.card', 'templates']);
 
-    resourceApp.config(['$stateProvider','$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    resourceApp.config(['$stateProvider','$urlRouterProvider','$qProvider', function ($stateProvider, $urlRouterProvider, $qProvider) {
+        $qProvider.errorOnUnhandledRejections(false);
         $stateProvider
-            .state('account',{
-                url:'/',
+            // .state('account',{
+            //     url:'/',
+            //     templateUrl:'test.html',
+            //     controller: 'accountCtrl'
+            // })
+            // .state('account.locations',{
+            //     url:'locations',
+            //     templateUrl:'test.html',
+            //     controller: 'accountCtrl'
+            // })
+            // .state('locations.location',{
+            //     url:'/{locationId}',
+            //     templateUrl:'test.html',
+            //     controller: 'accountCtrl'
+            // });
+            .state('people',{
+                url:'/people',
+                templateUrl:'test.html',
+                controller: 'accountCtrl'
+            })
+            .state('people.person',{
+                url:'/{personId}',
                 templateUrl:'test.html',
                 controller: 'accountCtrl'
             });
-        $urlRouterProvider.otherwise('/account');
-    }]);
+            // .state('people.person.billings',{
+            //     url:'/billings',
+            //     templateUrl:'test.html',
+            //     controller: 'accountCtrl'
+            // })
+            // .state('people.person.billings.billing',{
+            //     url:'/{billingId}',
+            //     templateUrl:'test.html',
+            //     controller: 'accountCtrl'
+            // });
 
+
+        // $urlRouterProvider.otherwise('/');
+    }]);
+// {tenantPath:[a-zA-Z0-9/]*}/user'
+
+    resourceApp.factory('suiDataServ', suiDataServ);
+    suiDataServ.$inject = ['$http'];
+    function suiDataServ($http) {
+        var suiDataServ = {
+            async: function() {
+                // $http returns a promise, which has a then function, which also returns a promise
+                var promise = $http.get('sui_account_mgmt_example.json').then(function (response) {
+                    // The then function here is an opportunity to modify the response
+                    // console.log(response);
+                    // The return value gets picked up by the then in the controller.
+                    return response.data;
+                });
+                // Return the promise to the controller
+                return promise;
+            }
+        };
+        return suiDataServ;
+    }
 
 
     resourceApp.controller('accountCtrl', accountCtrl);
-    accountCtrl.$inject = ['$scope','$routeParams','$location'];
-    function accountCtrl($scope, $routeParams, $location) {
-        console.log("Here");
-        $scope.node = null;
-        $http.get('sui_account_mgmt_example.json').success(function(data) {
-            $scope.node = data;
-            console.log($scope.node)
+    accountCtrl.$inject = ['$scope', 'suiDataServ','$state', '$stateParams'];
+    function accountCtrl($scope, suiDataServ, $state, $stateParams) {
+        suiDataServ.async().then(function(d) {
+            $scope.data = d;
+            console.log($state.current);
+            console.log($stateParams);
+            console.log($state.params);
         });
     }
 
