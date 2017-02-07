cfcfa6d2ea34341b97f60010a0f3ca1607f9754d
Alexander-Andrade <veimar.94@gmail.com>
2017-02-07 12:06:01 +0300

    resolve

--- app/assets/javascripts/account_view.js
+++ app/assets/javascripts/account_view.js
@@ -34,29 +34,27 @@
                 url:'/{path:.*}',
                 templateUrl:'test.html',
                 controller: 'accountCtrl',
-                // resolve:{
-                //     ctrlData:['$state', '$stateParams' ,function ($state, $stateParams) {
-                //         console.log("resolve ")
-                //     }]
-                // },
-                onEnter: ['$state','$stateParams','suiDataServ', function($state, $stateParams, suiDataServ){
-                    suiDataServ.async().then(function(d) {
-                        var path = $stateParams.path;
-                        console.log(path);
-                        var rootNode = d;
+                resolve:{
+                    suiData:['suiDataServ' ,function (suiDataServ) {
+                        return suiDataServ.getPromise();
+                    }]
+                },
+                onEnter: ['$state','$stateParams','suiData', function($state, $stateParams, suiData){
 
-                        // my tenant path
-                        if(path == 'undefined' || path.length== 0){
-                            suiDataServ.node = [rootNode];
-                            return;
-                        }
-                        // locations path
-                        if(path == "locations"){
-                            suiDataServ.node = rootNode.children;
-                        }
-                    });
-                    // var params = $stateParams.path.split('/');
-                    // console.log(params)
+                    var path = $stateParams.path;
+                    console.log(path);
+
+                    // my tenant path
+                    if(path == 'undefined' || path.length== 0){
+                        suiData.node = [suiData];
+                        return;
+                    }
+                    // locations path
+                    if(path == "locations"){
+                        suiData.node = suiData.children;
+                    }
+                    var params = $stateParams.path.split('/');
+
                 }]
             });
 
@@ -64,38 +62,34 @@
     }]);
 // {tenantPath:[a-zA-Z0-9/]*}/user'
 
-    resourceApp.factory('suiDataServ', suiDataServ);
+    resourceApp.service('suiDataServ', suiDataServ);
     suiDataServ.$inject = ['$http'];
     function suiDataServ($http) {
-        var suiDataServ = {
-            async: function() {
-                // $http returns a promise, which has a then function, which also returns a promise
-                var promise = $http.get('sui_account_mgmt_example.json').then(function (response) {
-                    // The then function here is an opportunity to modify the response
-                    // console.log(response);
-                    // The return value gets picked up by the then in the controller.
-                    return response.data;
-                });
-                // Return the promise to the controller
-                return promise;
+        //singleton (json only one time)
+        this.promise = null;
+
+        function makeRequest() {
+            // $http returns a promise, which has a then function, which also returns a promise
+            return $http.get('sui_account_mgmt_example.json').then(function (response) {
+                // The then function here is an opportunity to modify the response
+                // console.log(response);
+                // The return value gets picked up by the then in the controller.
+                return response.data;
+            });
+        }
+        this.getPromise = function (update) {
+            if(update || !this.promise){
+                console.log("request !!!");
+                this.promise = makeRequest();
             }
-        };
-        return suiDataServ;
+            return this.promise;
+        }
     }
 
-
     resourceApp.controller('accountCtrl', accountCtrl);
-    accountCtrl.$inject = ['$scope', 'suiDataServ', '$state', '$stateParams'];
-    function accountCtrl($scope, suiDataServ, $state, $stateParams) {
-        suiDataServ.async().then(function(d) {
-                console.log(suiDataServ.node);
-            // console.log(suiDataServ.node);
-            // console.log(parameters);
-            // console.log($state.current);
-            // console.log($stateParams);
-            // console.log($state.params);
-        });
-
+    accountCtrl.$inject = ['$scope', 'suiData', '$state', '$stateParams'];
+    function accountCtrl($scope, suiData, $state, $stateParams) {
+        $scope.data = suiData.node;
     }
 
 // })();
